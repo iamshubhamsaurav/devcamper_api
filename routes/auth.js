fe858f2647df650765e598c421979930a1faf8eb
Shubham Saurav <shubhamsinha186@gmail.com>
2020-08-02 15:47:18 +0530

    changed logout route to get

--- routes/auth.js
+++ routes/auth.js
@@ -16,7 +16,7 @@ const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
-router.post('/logout', logout);
+router.get('/logout', logout);
 
 router.get('/me', protect, getMe);
 // router.post('/forgotpassword', forgotpassword);
