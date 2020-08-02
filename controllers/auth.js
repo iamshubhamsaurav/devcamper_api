86bb9f864d1a70c1cc73835f0d128d410a9d1869
Shubham Saurav <shubhamsinha186@gmail.com>
2020-08-02 15:30:40 +0530

    Added Logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -43,6 +43,14 @@ exports.getMe = asyncHandler(async (req, res, next) => {
   res.status(200).json({ success: true, data: user });
 });
 
+exports.logout = asyncHandler(async (req, res, next) => {
+  res.cookie('token', 'none', {
+    expires: new Date.now() + 1000,
+    httpOnly: true,
+  });
+  res.status(200).json({ success: true, data: {} });
+});
+
 exports.forgotPassword = asyncHandler(async (req, res, next) => {
   const user = await User.findOne({ email: req.body.email });
   if (!user) {
