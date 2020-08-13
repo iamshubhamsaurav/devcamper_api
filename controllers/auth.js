8e378bc52acaa7cfbb6fc8d02efe7858ee1659a7
Shubham Saurav <shubhamsinha186@gmail.com>
2020-08-13 19:53:33 +0530

    Fixed millisecond calc in sendTokenResponse

--- controllers/auth.js
+++ controllers/auth.js
@@ -136,7 +136,7 @@ const sendTokenResponse = (user, statusCode, res) => {
   const token = user.getSignedJwtToken();
 
   const options = {
-    expires: new Date(Date.now() + 30 * 24 * 60 * 1000),
+    expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
     httpOnly: true,
   };
 
